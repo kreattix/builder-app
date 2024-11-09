30a8e9f793786850140d90b6f8cea1c9b1980d7e
Prince Patel <prpatel0949@gmail.com>
2024-11-09 19:13:18 +0530

    add html

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { PersistGate } from 'redux-persist/integration/react'
 
 import App from './App'
 import { persistedStore, store } from './app/store'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- src/pages/CustomLogin.tsx
+++ src/pages/CustomLogin.tsx
@@ -21,8 +21,20 @@ export const CustomLogin = () => {
   }
 
   return (
-    <button onClick={onLogin}>
-      {isAuthenticating ? 'Loading' : 'Login with Google Account'}
-    </button>
+    <div className="container">
+      <div className="logo">Kreattix Builder</div>
+      <h2>Unleash Your Creativity with Kreattix.</h2>
+      <p>
+        Sign in or create a new account with your Google account. It's quick, secure, and
+        hassle-free.
+      </p>
+      <button className="google-button" onClick={onLogin}>
+        {isAuthenticating ? 'Loading' : 'Continue with Google'}
+      </button>
+      <p>
+        New here? Click above to get started effortlessly with Google. Returning user?
+        Welcome back!
+      </p>
+    </div>
   )
 }
